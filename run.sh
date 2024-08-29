a4d67aac16b06af8b41afa98a41e088e05819d9f
tsukasakop <tsukasakop@gmail.com>
2024-08-29 13:10:09 +0900

    🧑‍💻 Automatically start docker daemon

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 IMG="42tokyo-clang-env"
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  docker ps >/dev/null || open -a /Application/Docker.app
+fi
+
 docker run --platform linux/amd64 -it --rm -v $(pwd):/workspace $IMG
